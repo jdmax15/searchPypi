7ed56de71b9c3fda8e71edadb8abce999168b873
jdmax15 <joel.maxwell15@gmail.com>
2024-11-03 10:51:36 +1100

    Added top search result link selector

--- searchpypi.py
+++ searchpypi.py
@@ -8,6 +8,8 @@ res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q='
                    + ' '.join(sys.argv[1]))
 res.raise_for_status()
 
-# TODO: Retrieve top search result links.
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-# TODO: Open a browser tab for each result.
+# Open a browser tab for each result.
+linkElems = soup.select('.package-snippet')
