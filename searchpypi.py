16f2c4d58e7f4029e3b7143d2150f94977cb92ba
jdmax15 <joel.maxwell15@gmail.com>
2024-11-03 10:41:27 +1100

    Initial commit

---
+++ searchpypi.py
@@ -0,0 +1,13 @@
+#! python3
+# searchpypi.py     - Opens several search results.
+
+import requests, sys, webbrowser, bs4
+
+print('Searching...')   # display text while downloading the search result page
+res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q='
+                   + ' '.join(sys.argv[1]))
+res.raise_for_status()
+
+# TODO: Retrieve top search result links.
+
+# TODO: Open a browser tab for each result.
